f7260e447aaacbb118d0c17c2046f1d9be31d93a
Kintas17 <49915294+Kintas17@users.noreply.github.com>
2019-12-05 11:57:34 +0000

    Add files via upload

---
+++ Euro/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Euro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int nestrela = 0, nnumeros = 0;        
+        
+        
+
+        
+
+        private void estrela(object sender, EventArgs e)
+        {
+            Label temp = (Label)sender;
+            if (temp.Text == "X")
+            {
+                temp.Text = "";
+                nestrela--;
+            }
+            else if (nestrela<2)
+            {
+                temp.Text = "X";
+                nestrela++;
+            }
+            teste.Text = temp.Tag.ToString();
+        }
+
+        private void numeros(object sender, EventArgs e)
+        {
+            Label temp = (Label)sender;
+            if (temp.Text == "X")
+            {
+                temp.Text = "";
+                nnumeros--;
+            }
+            else if (nnumeros < 5)
+            {
+                temp.Text = "X";
+                nnumeros++;7
+            }
+            teste.Text = temp.Tag.ToString();
+        }
+
+        private void Label63_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Euro/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Euro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Euro")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("a4ee8c9d-93e4-4a2e-b66b-8c9ae4356e48")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
